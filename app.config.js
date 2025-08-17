34a1c6c8f4840f1d6a1f4a6b1b3f863ea04837c1
LatidoIA <sebaurream@gmail.com>
2025-08-17 17:23:50 -0400

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,86 +1,68 @@
 // app.config.js
-const { withAndroidManifest, withProjectBuildGradle } = require("@expo/config-plugins");
+const { withPlugins, withProjectBuildGradle } = require('@expo/config-plugins');
 
-// 🔧 Patch Manifest: evita choque de appComponentFactory
-const withFixAppComponentFactory = (config) =>
-  withAndroidManifest(config, (cfg) => {
-    const manifest = cfg.modResults.manifest;
-    manifest.$["xmlns:tools"] = manifest.$["xmlns:tools"] || "http://schemas.android.com/tools";
-    const app = manifest.application?.[0];
-    if (app) {
-      app.$["android:appComponentFactory"] = "androidx.core.app.CoreComponentFactory";
-      const list = new Set(
-        (app.$["tools:replace"] ? String(app.$["tools:replace"]).split(",") : [])
-          .map((s) => s.trim())
-          .filter(Boolean)
-      );
-      list.add("android:appComponentFactory");
-      app.$["tools:replace"] = Array.from(list).join(",");
-    }
-    return cfg;
-  });
-
-// 🧹 EXCLUIR legacy Support libs (la causa de las clases duplicadas)
-const withStripSupportLibs = (config) =>
-  withProjectBuildGradle(config, (cfg) => {
-    const mod = cfg.modResults;
-    if (mod.language !== "groovy") return cfg;
-
-    const block = `
-configurations.all {
-  exclude group: 'com.android.support'
-  exclude group: 'com.android.support', module: 'support-compat'
-  exclude group: 'com.android.support', module: 'support-v4'
-  exclude group: 'com.android.support', module: 'support-media-compat'
-  exclude group: 'com.android.support', module: 'support-annotations'
-  exclude group: 'com.android.support', module: 'versionedparcelable'
+// Quita cualquier dependencia del grupo com.android.support en todos los subproyectos
+function withStripSupportLibs(config) {
+  return withProjectBuildGradle(config, (conf) => {
+    const snippet = `
+/** Strip legacy support libs that conflict with AndroidX */
+subprojects {
+  configurations.all {
+    exclude group: 'com.android.support'
+  }
 }
 `;
-    if (!mod.contents.includes("exclude group: 'com.android.support'")) {
-      mod.contents += `
-
-${block}
-`;
+    if (!conf.modResults.contents.includes("exclude group: 'com.android.support'")) {
+      conf.modResults.contents += `\n${snippet}\n`;
     }
-    return cfg;
+    return conf;
   });
+}
 
-module.exports = {
-  name: "LATIDO",
-  slug: "latido",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./icono.png",
-  scheme: "latido",
-  userInterfaceStyle: "automatic",
-  splash: {
-    image: "./splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#000000",
-  },
-  android: {
-    package: "com.latido.app",
-    versionCode: 3,
-    adaptiveIcon: {
-      foregroundImage: "./adaptive-icon.png",
-      backgroundColor: "#000000",
+module.exports = ({ config }) =>
+  withPlugins(
+    {
+      name: "LATIDO",
+      slug: "latido",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./icono.png",
+      scheme: "latido",
+      userInterfaceStyle: "automatic",
+      splash: { image: "./splash.png", resizeMode: "contain", backgroundColor: "#000000" },
+      android: {
+        package: "com.latido.app",
+        versionCode: 3,
+        adaptiveIcon: { foregroundImage: "./adaptive-icon.png", backgroundColor: "#000000" },
+        permissions: [
+          "android.permission.health.READ_STEPS",
+          "android.permission.health.READ_HEART_RATE"
+        ],
+        minSdkVersion: 26,
+        targetSdkVersion: 35
+      },
+      plugins: [
+        "expo-health-connect",
+        [
+          "expo-build-properties",
+          {
+            android: {
+              compileSdkVersion: 35,
+              targetSdkVersion: 35,
+              minSdkVersion: 26,
+              // <- IMPORTANTÍSIMO para convertir dependencias heredadas a AndroidX
+              gradleProperties: {
+                "android.useAndroidX": "true",
+                "android.enableJetifier": "true"
+              }
+            }
+          }
+        ]
+      ],
+      sdkVersion: "53.0.0",
+      platforms: ["ios", "android"],
+      // Si EAS te lo pidió, asegurate de tener:
+      // extra: { eas: { projectId: "<TU_PROJECT_ID_DE_EAS>" } }
     },
-    permissions: [
-      "android.permission.health.READ_STEPS",
-      "android.permission.health.READ_HEART_RATE",
-    ],
-    minSdkVersion: 26,
-    targetSdkVersion: 35,
-  },
-  plugins: [
-    "expo-health-connect",
-    ["expo-build-properties", { android: { compileSdkVersion: 35, targetSdkVersion: 35, minSdkVersion: 26 } }],
-    withFixAppComponentFactory,
-    withStripSupportLibs, // 👈 aquí quitamos com.android.support*
-  ],
-  extra: {
-    eas: { projectId: "2ac93018-3731-4e46-b345-6d54a5502b8f" }, // usa tu ID real
-  },
-  sdkVersion: "53.0.0",
-  platforms: ["ios", "android"],
-};
+    [withStripSupportLibs]
+  );
