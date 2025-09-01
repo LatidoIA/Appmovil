001823a1b45348f5799b22cdc35bdade9b915382
LatidoIA <sebaurream@gmail.com>
2025-09-01 17:34:34 -0400

    Create react-native.config.js

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+// react-native.config.js
+module.exports = {
+  dependencies: {
+    'react-native-svg': {
+      platforms: { android: null, ios: null },
+    },
+    'react-native-health-connect': {
+      platforms: { android: null, ios: null }, // por si quedó instalado
+    },
+  },
+};
