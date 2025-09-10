79adbddb360f293efa54cee7fd63dfe9ef3606bf
LatidoIA <sebaurream@gmail.com>
2025-09-10 16:58:14 -0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // ¡Reanimated SIEMPRE al final!
+      // Reanimated SIEMPRE al final
       'react-native-reanimated/plugin',
     ],
   };
