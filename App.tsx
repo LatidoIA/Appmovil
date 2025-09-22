3a7f4d0a6594961d934f3b27625d9ff0c28bc8ee
LatidoIA <sebaurream@gmail.com>
2025-09-22 09:05:02 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-// App.tsx
+import 'react-native-gesture-handler';
 import React, { useCallback, useEffect, useState } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
