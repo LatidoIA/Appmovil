42bfe08e41a525d2ffb541cdc61f769d29a9185b
LatidoIA <sebaurream@gmail.com>
2025-11-13 12:27:34 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,23 +1,16 @@
-// Debe ser la primera importación
+// App.tsx
 import 'react-native-gesture-handler';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useFonts, Montserrat_400Regular, Montserrat_600SemiBold } from '@expo-google-fonts/montserrat';
-import RootNavigator from './src/navigation/RootNavigator';
-import { AuthProvider } from './src/auth/AuthContext';
+import { NavigationContainer } from '@react-navigation/native';
+import AppTabs from './src/navigation/AppTabs';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Montserrat_400Regular,
-    Montserrat_600SemiBold,
-  });
-  if (!fontsLoaded) return null;
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <AuthProvider>
-        <RootNavigator />
-      </AuthProvider>
+      <NavigationContainer>
+        <AppTabs />
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
