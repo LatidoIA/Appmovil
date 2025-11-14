79272bcdc68ca63519b02b116f637c6c0df92239
LatidoIA <sebaurream@gmail.com>
2025-11-14 09:37:10 -0300

    Remove AuthProvider from App component
    Removed AuthProvider wrapping around AppTabs.

--- App.tsx
+++ App.tsx
@@ -3,18 +3,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-
 import AppTabs from './src/navigation/AppTabs';
-import { AuthProvider } from './src/context/AuthContext';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
-        {/* Mantenemos el contexto de auth para que useAuth no reviente */}
-        <AuthProvider>
-          <AppTabs />
-        </AuthProvider>
+        <AppTabs />
       </NavigationContainer>
     </GestureHandlerRootView>
   );
