c3e0d64b91467581b8473bb8eb0bdf1ce6a46189
LatidoIA <sebaurream@gmail.com>
2025-11-14 09:19:56 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,13 +3,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+
 import AppTabs from './src/navigation/AppTabs';
+import { AuthProvider } from './src/context/AuthContext';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
-        <AppTabs />
+        {/* Mantenemos el contexto de auth para que useAuth no reviente */}
+        <AuthProvider>
+          <AppTabs />
+        </AuthProvider>
       </NavigationContainer>
     </GestureHandlerRootView>
   );
